f267b7ef8a889649e13c308ce359cbc956f98698
Guewen Le Bechennec <guewen.lebechennec147@gmail.com>
2022-10-25 15:02:59 +0200

    Classe Ingredient

--- src/main/java/Ingredient.java
+++ src/main/java/Ingredient.java
@@ -1,4 +1,30 @@
 
 public class Ingredient {
+	 private String libelle;
 
+	/**
+	 * Constructeur
+	 * @param libelle
+	 */
+	public Ingredient(String libelle) {
+		this.libelle = libelle;
+	}
+
+	/**
+	 * @return the libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/**
+	 * @param libelle the libelle to set
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
+	 
+	 
 }
